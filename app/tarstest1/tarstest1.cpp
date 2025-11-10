54caa89d08286148309cd58908bb415fe71de0cc
cyberbougnat <nicodemus63000@gmail.com>
2025-11-11 00:55:06 +0100

    toto

---
+++ app/tarstest1/tarstest1.cpp
@@ -0,0 +1,12 @@
+﻿// tarstest1.cpp : définit le point d'entrée de l'application.
+//
+#include "C:\Users\arnau\TARS\WiringPi\wiringPi\wiringPi.h"
+#include "tarstest1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
