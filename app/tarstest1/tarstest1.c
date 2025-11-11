0177536855affbf96e5fca98ef06a4085535dd15
Skinson <peaudechamois@hotmail.com>
2025-11-11 11:43:42 +0100

    with loop

--- app/tarstest1/tarstest1.c
+++ app/tarstest1/tarstest1.c
@@ -1,13 +1,23 @@
 ﻿// tarstest1.cpp : définit le point d'entrée de l'application.
 //
 #include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
 #include "/home/tars/test2/WiringPi/wiringPi/wiringPi.h"
 #include "tarstest1.h"
+
 
 using namespace std;
 
 int main()
 {
-	fprintf (stdout, "Hello CMake.");
+	signal(SIGINT, sig_handler);
+	fprintf (stdout, "Hello Tarsouilleurs !\n");
 	return 0;
+}
+
+void sig_handler(int num) {
+    if (num == SIGINT) {
+        running = false;
+    }
 }
--- app/tarstest1/tarstest1.h
+++ app/tarstest1/tarstest1.h
@@ -6,3 +6,4 @@
 #include <iostream>
 
 // TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
+void sig_handler(int);
