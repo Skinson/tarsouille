fbcc78547f254cbaf71e364b671c2608a9014083
Skinson <peaudechamois@hotmail.com>
2025-11-11 01:14:20 +0100

    toto

--- app/tarstest1/tarstest1.cpp
+++ app/tarstest1/tarstest1.cpp
@@ -1,6 +1,6 @@
 ﻿// tarstest1.cpp : définit le point d'entrée de l'application.
 //
-#include "C:\Users\arnau\TARS\WiringPi\wiringPi\wiringPi.h"
+#include "~/test2/WiringPi/wiringPi/wiringPi.h"
 #include "tarstest1.h"
 
 using namespace std;
